c17744886ffc379a85ba2ac0b1cbc20ab1f58673
Neeeraj0 <neeraj.patil@circolife.com>
2026-09-23 21:31:08 +0530

    (fix) allowed origin fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-8gt5y_wuw9rnibe0z(^m&5((hg3=ed22x2s4mr07e5=2g3qrtu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://spotterlabsassignment.onrender.com/"
+]
 
 
 # Application definition
